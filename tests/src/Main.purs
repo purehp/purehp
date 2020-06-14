d41a1728c829aed131acd8c64f91d09201a22ec0
Dario Oddenino <branch13@gmail.com>
2020-06-15 00:54:05 +0200

    object rework

--- tests/src/Main.purs
+++ tests/src/Main.purs
@@ -2,25 +2,32 @@ module Main where
 
 -- import Prelude
 
-int1 :: Int
-int1 = 5
+-- int1 :: Int
+-- int1 = 5
 
-int2 :: Int
-int2 = int1
+-- int2 :: Int
+-- int2 = int1
 
-string1 :: String
-string1 = "Hello World!"
+-- string1 :: String
+-- string1 = "Hello World!"
 
-char1 :: Char
-char1 = 'C'
+-- char1 :: Char
+-- char1 = 'C'
 
-array1 :: Array Int
-array1 = [1, 2, 3, 4]
+-- array1 :: Array Int
+-- array1 = [1, 2, 3, 4]
 
-fun1 :: Int -> Int
-fun1 arg = arg
+-- fun1 :: Int -> Int
+-- fun1 arg = arg
 
--- data Unit = Unit
+rec1 :: { x :: Int, y :: String }
+rec1 = { x: 1, y: "Hello" }
+
+-- newtype Foo = Foo Int
+
+-- data Bar = A Int | B String | C
+
+-- data Foo = Foo Int
 
 -- data Unit = Unit
 
