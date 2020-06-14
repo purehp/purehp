91c41682c032d53766749fce8b3c6fd2bd6b8829
Dario Oddenino <branch13@gmail.com>
2020-06-14 12:44:17 +0200

    updated ast and commons

--- src/Language/PureScript/PHP/CodeGen/AST.hs
+++ src/Language/PureScript/PHP/CodeGen/AST.hs
@@ -1,6 +1,3 @@
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE PatternSynonyms #-}
-
 -- |
 -- Data types for the intermediate simplified-PHP AST
 --
@@ -8,61 +5,267 @@ module Language.PureScript.PHP.CodeGen.AST where
 
 import Prelude.Compat
 
+import Control.Monad ((>=>))
+import Control.Monad.Identity (Identity(..), runIdentity)
 import Data.Text (Text)
 
-import Control.Monad.Identity
-import Control.Arrow (second)
-
+import Language.PureScript.AST (SourceSpan(..))
+import Language.PureScript.Comments
 import Language.PureScript.PSString (PSString)
-import Language.PureScript.AST.SourcePos
+import Language.PureScript.Traversals
+
+-- | Built-in unary operators
+-- Copying js for now
+data UnaryOperator
+  = Negate
+  | Not
+  | BitwiseNot
+  | Positive
+  | New
+  deriving (Show, Eq)
+
+-- | Built-in binary operators
+-- Copying js for now
+data BinaryOperator
+  = Add
+  | Subtract
+  | Multiply
+  | Modulus
+  | EqualsTo
+  | NotEqualTo
+  | LessThan
+  | LessThanOrEqualTo
+  | GreaterThan
+  | GreaterThanOrEqualTo
+  | And
+  | Or
+  | BitwiseAnd
+  | BitwiseOr
+  | BitwiseXor
+  | ShiftLeft
+  | ShiftRight
+  | ZeroFillShiftRight
+  deriving (Show, Eq)
 
 data PHP
-  -- | A numeric literal
-  = PNumericLiteral (Either Integer Double)
-  -- | A string literal
-  | PStringLiteral PSString
-  -- | Top-level function definition
-  | PFunctionDef (Maybe SourceSpan) [Text] PHP
-  -- | Variable bind
-  | PVarBind Text PHP
-  -- | A variable
-  | PVar Text
-  -- | A function reference f/1
-  | PFunRef PSString Int
-  -- | A fun definition
-  | PFunFull (Maybe Text) [(PFunBinder, PHP)]
-  -- | Function application
-  | PApp PHP [PHP]
-  -- | Block
-  | PBlock [PHP]
-  -- | An array
-  | PArrayLiteral [PHP]
-  -- | An associative array
-  | PAssociativeArrayLiteral [(PSString, PHP)]
-
+  = PNumericLiteral (Maybe SourceSpan) (Either Integer Double)
+  -- ^ A numeric literal
+  | PStringLiteral (Maybe SourceSpan) PSString
+  -- ^ A string literal
+  | PBooleanLiteral (Maybe SourceSpan) Bool
+  -- ^ A boolean literal
+  | PUnary (Maybe SourceSpan) UnaryOperator PHP
+  -- ^ A unary operator application
+  | PBinary (Maybe SourceSpan) BinaryOperator PHP PHP
+  -- ^ A binary operator application
+  | PArrayLiteral (Maybe SourceSpan) [PHP]
+  -- ^ An array literal
+  | PIndexer (Maybe SourceSpan) PHP PHP
+  -- ^ An array indexer expression
+  | PClassLiteral (Maybe SourceSpan) Text [(PSString, PHP)]
+  -- ^ A class literal (for records?), the text is to store the name. tbd
+  | PFunction (Maybe SourceSpan) (Maybe Text) [Text] PHP
+  -- ^ A function introduction (optional name, arguments, body)
+  | PApp (Maybe SourceSpan) PHP [PHP]
+  -- ^ Function application
+  | PVar (Maybe SourceSpan) Text
+  -- ^ Variable
+  | PBlock (Maybe SourceSpan) [PHP]
+  -- ^ A block of expressions in braces
+  | PVariableIntroduction (Maybe SourceSpan) Text (Maybe PHP)
+  -- ^ A variable introduction and optional initialization
+  | PAssignment (Maybe SourceSpan) PHP PHP
+  -- ^ A variable assignment
+  -- while
+  -- for
+  -- forin
+  -- ifelse
+  | PReturn (Maybe SourceSpan) PHP
+  -- ^ Return statement
+  | PReturnNoResult (Maybe SourceSpan)
+  -- ^ Return statement with no return value
+  -- throw
+  -- instanceof
+  | PComment (Maybe SourceSpan) [Comment] PHP
+  -- ^ Commented php
   deriving (Show, Eq)
 
--- | Simple 0-arity version of PFun1
-pattern PFun0 :: Maybe Text -> PHP -> PHP
-pattern PFun0 name e = PFunFull name [(PFunBinder [], e)]
+withSourceSpan :: SourceSpan -> PHP -> PHP
+withSourceSpan withSpan = go where
+  ss :: Maybe SourceSpan
+  ss = Just withSpan
 
--- | Simple fun definition fun f(X) -> e end (arity 1 with single head with simple variable pattern, name optional)
-pattern PFun1 :: Maybe Text -> Text -> PHP -> PHP
-pattern PFun1 name var e = PFunFull name [(PFunBinder [PVar var], e)]
+  go :: PHP -> PHP
+  go (PNumericLiteral _ n) = PNumericLiteral ss n
+  go (PStringLiteral _ s) = PStringLiteral ss s
+  go (PBooleanLiteral _ b) = PBooleanLiteral ss b
+  go (PUnary _ op j) = PUnary ss op j
+  go (PBinary _ op j1 j2) = PBinary ss op j1 j2
+  go (PArrayLiteral _ js) = PArrayLiteral ss js
+  go (PIndexer _ j1 j2) = PIndexer ss j1 j2
+  go (PClassLiteral _ n js) = PClassLiteral ss n js
+  go (PFunction _ name args j) = PFunction ss name args j
+  go (PApp _ j js) = PApp ss j js
+  go (PVar _ s) = PVar ss s
+  go (PBlock _ js) = PBlock ss js
+  go (PVariableIntroduction _ name j) = PVariableIntroduction ss name j
+  go (PAssignment _ j1 j2) = PAssignment ss j1 j2
+  -- go (While _ j1 j2) = While ss j1 j2
+  -- go (For _ name j1 j2 j3) = For ss name j1 j2 j3
+  -- go (ForIn _ name j1 j2) = ForIn ss name j1 j2
+  -- go (IfElse _ j1 j2 j3) = IfElse ss j1 j2 j3
+  go (PReturn _ js) = PReturn ss js
+  go (PReturnNoResult _) = PReturnNoResult ss
+  -- go (Throw _ js) = Throw ss js
+  -- go (InstanceOf _ j1 j2) = InstanceOf ss j1 j2
+  go (PComment _ com j) = PComment ss com j
 
-extractVars :: [PHP] -> Maybe [Text]
-extractVars = traverse var
-  where var (PVar x) = Just x
-        var _ = Nothing
 
--- | Simple arity-N versions of PFun1
-pattern PFunN :: Maybe Text -> [Text] -> PHP -> PHP
-pattern PFunN name vars e <- PFunFull name [(PFunBinder (extractVars -> Just vars), e)] where
-  PFunN name vars e = PFunFull name [(PFunBinder (map PVar vars), e)]
+getSourceSpan :: PHP -> Maybe SourceSpan
+getSourceSpan = go where
+  go :: PHP -> Maybe SourceSpan
+  go (PNumericLiteral ss _) = ss
+  go (PStringLiteral ss _) = ss
+  go (PBooleanLiteral ss _) = ss
+  go (PUnary ss _ _) = ss
+  go (PBinary ss _ _ _) = ss
+  go (PArrayLiteral ss _) = ss
+  go (PIndexer ss _ _) = ss
+  go (PClassLiteral ss _ _) = ss
+  go (PFunction ss _ _ _) = ss
+  go (PApp ss _ _) = ss
+  go (PVar ss _) = ss
+  go (PBlock ss _) = ss
+  go (PVariableIntroduction ss _ _) = ss
+  go (PAssignment ss _ _) = ss
+  -- go (While ss _ _) = ss
+  -- go (For ss _ _ _ _) = ss
+  -- go (ForIn ss _ _ _) = ss
+  -- go (IfElse ss _ _ _) = ss
+  go (PReturn ss _) = ss
+  go (PReturnNoResult ss) = ss
+  -- go (Throw ss _) = ss
+  -- go (InstanceOf ss _ _) = ss
+  go (PComment ss _ _) = ss
 
-data PFunBinder
-  = PFunBinder [PHP]
-  deriving (Show, Eq)
 
-data Guard = Guard PHP
-  deriving (Show, Eq)
+everywhere :: (PHP -> PHP) -> PHP -> PHP
+everywhere f = go where
+  go :: PHP -> PHP
+  go (PUnary ss op j) = f (PUnary ss op (go j))
+  go (PBinary ss op j1 j2) = f (PBinary ss op (go j1) (go j2))
+  go (PArrayLiteral ss js) = f (PArrayLiteral ss (map go js))
+  go (PIndexer ss j1 j2) = f (PIndexer ss (go j1) (go j2))
+  go (PClassLiteral ss n js) = f (PClassLiteral ss n (map (fmap go) js))
+  go (PFunction ss name args j) = f (PFunction ss name args (go j))
+  go (PApp ss j js) = f (PApp ss (go j) (map go js))
+  go (PBlock ss js) = f (PBlock ss (map go js))
+  go (PVariableIntroduction ss name j) = f (PVariableIntroduction ss name (fmap go j))
+  go (PAssignment ss j1 j2) = f (PAssignment ss (go j1) (go j2))
+  -- go (While ss j1 j2) = f (While ss (go j1) (go j2))
+  -- go (For ss name j1 j2 j3) = f (For ss name (go j1) (go j2) (go j3))
+  -- go (ForIn ss name j1 j2) = f (ForIn ss name (go j1) (go j2))
+  -- go (IfElse ss j1 j2 j3) = f (IfElse ss (go j1) (go j2) (fmap go j3))
+  go (PReturn ss js) = f (PReturn ss (go js))
+  -- go (Throw ss js) = f (Throw ss (go js))
+  -- go (InstanceOf ss j1 j2) = f (InstanceOf ss (go j1) (go j2))
+  go (PComment ss com j) = f (PComment ss com (go j))
+  go other = f other
+
+everywhereTopDown :: (PHP -> PHP) -> PHP -> PHP
+everywhereTopDown f = runIdentity . everywhereTopDownM (Identity . f)
+
+everywhereTopDownM :: (Monad m) => (PHP -> m PHP) -> PHP -> m PHP
+everywhereTopDownM f = f >=> go where
+  f' = f >=> go
+  go (PUnary ss op j) = PUnary ss op <$> f' j
+  go (PBinary ss op j1 j2) = PBinary ss op <$> f' j1 <*> f' j2
+  go (PArrayLiteral ss js) = PArrayLiteral ss <$> traverse f' js
+  go (PIndexer ss j1 j2) = PIndexer ss <$> f' j1 <*> f' j2
+  go (PClassLiteral ss n js) = PClassLiteral ss n <$> traverse (sndM f') js
+  go (PFunction ss name args j) = PFunction ss name args <$> f' j
+  go (PApp ss j js) = PApp ss <$> f' j <*> traverse f' js
+  go (PBlock ss js) = PBlock ss <$> traverse f' js
+  go (PVariableIntroduction ss name j) = PVariableIntroduction ss name <$> traverse f' j
+  go (PAssignment ss j1 j2) = PAssignment ss <$> f' j1 <*> f' j2
+  -- go (While ss j1 j2) = While ss <$> f' j1 <*> f' j2
+  -- go (For ss name j1 j2 j3) = For ss name <$> f' j1 <*> f' j2 <*> f' j3
+  -- go (ForIn ss name j1 j2) = ForIn ss name <$> f' j1 <*> f' j2
+  -- go (IfElse ss j1 j2 j3) = IfElse ss <$> f' j1 <*> f' j2 <*> traverse f' j3
+  go (PReturn ss j) = PReturn ss <$> f' j
+  -- go (Throw ss j) = Throw ss <$> f' j
+  -- go (InstanceOf ss j1 j2) = InstanceOf ss <$> f' j1 <*> f' j2
+  go (PComment ss com j) = PComment ss com <$> f' j
+  go other = f other
+
+everything :: (r -> r -> r) -> (PHP -> r) -> PHP -> r
+everything (<>.) f = go where
+  go j@(PUnary _ _ j1) = f j <>. go j1
+  go j@(PBinary _ _ j1 j2) = f j <>. go j1 <>. go j2
+  go j@(PArrayLiteral _ js) = foldl (<>.) (f j) (map go js)
+  go j@(PIndexer _ j1 j2) = f j <>. go j1 <>. go j2
+  go j@(PClassLiteral _ _ js) = foldl (<>.) (f j) (map (go . snd) js)
+  go j@(PFunction _ _ _ j1) = f j <>. go j1
+  go j@(PApp _ j1 js) = foldl (<>.) (f j <>. go j1) (map go js)
+  go j@(PBlock _ js) = foldl (<>.) (f j) (map go js)
+  go j@(PVariableIntroduction _ _ (Just j1)) = f j <>. go j1
+  go j@(PAssignment _ j1 j2) = f j <>. go j1 <>. go j2
+  -- go j@(While _ j1 j2) = f j <>. go j1 <>. go j2
+  -- go j@(For _ _ j1 j2 j3) = f j <>. go j1 <>. go j2 <>. go j3
+  -- go j@(ForIn _ _ j1 j2) = f j <>. go j1 <>. go j2
+  -- go j@(IfElse _ j1 j2 Nothing) = f j <>. go j1 <>. go j2
+  -- go j@(IfElse _ j1 j2 (Just j3)) = f j <>. go j1 <>. go j2 <>. go j3
+  go j@(PReturn _ j1) = f j <>. go j1
+  -- go j@(Throw _ j1) = f j <>. go j1
+  -- go j@(InstanceOf _ j1 j2) = f j <>. go j1 <>. go j2
+  go j@(PComment _ _ j1) = f j <>. go j1
+  go other = f other
+
+
+-- old stuff
+
+  -- -- | Top-level function definition
+  -- | PFunctionDef (Maybe SourceSpan) [Text] PHP
+  -- -- | Variable bind
+  -- | PVarBind Text PHP
+  -- -- | A variable
+  -- | PVar Text
+  -- -- | A function reference f/1
+  -- | PFunRef PSString Int
+  -- -- | A fun definition
+  -- | PFunFull (Maybe Text) [(PFunBinder, PHP)]
+  -- -- | Function application
+  -- | PApp PHP [PHP]
+  -- -- | Block
+  -- | PBlock [PHP]
+  -- -- | An array
+  -- | PArrayLiteral [PHP]
+  -- -- | An associative array
+  -- | PAssociativeArrayLiteral [(PSString, PHP)]
+
+  -- deriving (Show, Eq)
+
+-- -- | Simple 0-arity version of PFun1
+-- pattern PFun0 :: Maybe Text -> PHP -> PHP
+-- pattern PFun0 name e = PFunFull name [(PFunBinder [], e)]
+
+-- -- | Simple fun definition fun f(X) -> e end (arity 1 with single head with simple variable pattern, name optional)
+-- pattern PFun1 :: Maybe Text -> Text -> PHP -> PHP
+-- pattern PFun1 name var e = PFunFull name [(PFunBinder [PVar var], e)]
+
+-- extractVars :: [PHP] -> Maybe [Text]
+-- extractVars = traverse var
+--   where var (PVar x) = Just x
+--         var _ = Nothing
+
+-- -- | Simple arity-N versions of PFun1
+-- pattern PFunN :: Maybe Text -> [Text] -> PHP -> PHP
+-- pattern PFunN name vars e <- PFunFull name [(PFunBinder (extractVars -> Just vars), e)] where
+--   PFunN name vars e = PFunFull name [(PFunBinder (map PVar vars), e)]
+
+-- data PFunBinder
+--   = PFunBinder [PHP]
+--   deriving (Show, Eq)
+
+-- data Guard = Guard PHP
+--   deriving (Show, Eq)
--- src/Language/PureScript/PHP/CodeGen/Common.hs
+++ src/Language/PureScript/PHP/CodeGen/Common.hs
@@ -1,47 +1,94 @@
 -- |
 -- Common code generation utility functions
 --
-module Language.PureScript.PHP.CodeGen.Common
---( runAtom
--- , atomPS
--- , atom
--- , atomModuleName
-( phpModuleName
-, ModuleType(..)
--- , toAtomName
--- , toVarName
--- , identToAtomName
-, identToVar
--- , nameIsErlReserved
--- , utf8Binary
--- , encodeChar
-) where
+module Language.PureScript.PHP.CodeGen.Common where
 
-import           Data.Char
-import           Data.Monoid                         ((<>))
-import           Data.Text                           (Text, all, cons,
-                                                      intercalate, pack,
-                                                      singleton, uncons)
-import qualified Data.Text                           as T
-import           Data.Word                           (Word16)
-import           Language.PureScript.Names
-import           Language.PureScript.PSString
-import           Numeric
-import           Prelude.Compat                      hiding (all)
+import Data.Char
+import Data.Text (Text)
+import qualified Data.Text as T
 
-import           Language.PureScript.PHP.CodeGen.AST
+import Language.PureScript.Crash
+import Language.PureScript.Names
 
 
-data ModuleType = ForeignModule | PureScriptModule
+-- | Creates a PHP module name
+-- TODO: check that prefix thing
+moduleNameToPHP :: ModuleName -> Text
+moduleNameToPHP (ModuleName pns) =
+  let name = T.intercalate "\\" (runProperName `map` pns)
+  in if nameIsPHPBuiltIn name then "__" <> name else name
 
-phpModuleName :: ModuleName -> ModuleType -> Text
-phpModuleName (ModuleName pns) moduleType = pns  -- intercalate "_" ((toAtomName) `map` pns) <>
-  -- case moduleType of
-  --   ForeignModule    -> "@foreign"
-  --   PureScriptModule -> "@ps"
+-- | Convert an 'Ident' into a valid PHP identifier:
+--
+-- * Alphanumeric characters are kept unmodified.
+--
+-- * Reserved php identifiers are prefixed with '__'
+identToPHP :: Ident -> Text
+identToPHP (Ident name) = anyNameToPHP name
+identToPHP (GenIdent _ _) = internalError "GenIdent in identToPHP"
+identToPHP UnusedIdent = "$__unused"
 
-toVarName :: Text -> Text
-toVarName v = T.pack $ "$" ++ T.unpack v
+-- | Convert a 'ProperName' into a valid JavaScript identifier:
+--
+-- * Alphanumeric characters are kept unmodified.
+--
+-- * Reserved javascript identifiers are prefixed with '__'
+properToPHP :: ProperName a -> Text
+properToPHP = anyNameToPHP . runProperName
 
-identToVar :: Ident -> Text
-identToVar = toVarName . runIdent
+-- | Convert any name into a valid PHP identifier.
+--
+-- Note that this function assumes that the argument is a valid PureScript
+-- identifier (either an 'Ident' or a 'ProperName') to begin with; as such it
+-- will not produce valid JavaScript identifiers if the argument e.g. begins
+-- with a digit. Prefer 'identToPHP' or 'properToPHP' where possible.
+anyNameToPHP :: Text -> Text
+anyNameToPHP name
+  | nameIsPHPReseved name || nameIsPHPBuiltIn name = "__" <> name
+  | otherwise = T.concatMap identCharToText name
+
+-- | Test if a string is a valid PHP identifier as-is. Note that, while
+-- a return value of 'True' guarantees that the string is a valid PHP
+-- identifier, a return value of 'False' does not guarantee that the string is
+-- not a valid PHP identifier. That is, this check is more conservative than
+-- absolutely necessary.
+isValidPHPIdentifier :: Text -> Bool
+isvalidPHPIdentifier s =
+  and
+    [ not (T.null s)
+    , isAlpha (T.head s)
+    , s == anyNameToPHP s
+    ]
+
+-- | Attempts to find a human-readable name for a symbol, if none has been specified returns the
+-- ordinal value.
+-- TODO: See if and what's actually necessary here.
+identCharToText :: Char -> Text
+identCharToText c | isAlphaNum c = T.singleton c
+identCharToText c = '_' `T.cons` T.pack(show (ord c))
+
+-- | Checks whether an identifier name is reserved in PHP.
+nameIsPHPReserved :: Text -> Bool
+nameIsPHPReserved name =
+  name `elem` phpAnyReserved
+
+-- | Checks whether a name matches a built-in value in PHP.
+-- TODO: create the list
+nameIsPHPBuiltIn :: Text -> Bool
+nameIsPHPBuiltIn name =
+  name `elem`
+  []
+
+-- TODO: Create the list
+phpAnyReserved :: [Text]
+phpAnyReserved =
+  concat
+  []
+
+-- old
+
+-- toVarName :: Text -> Text
+-- toVarName v = T.pack $ "$" ++ T.unpack v
+
+-- identToVar :: Ident -> Text
+-- identToVar = toVarName . runIdent
