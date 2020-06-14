540a5a28756c5e0f3a76acf9d4853778d1e80edc
Dario Oddenino <branch13@gmail.com>
2020-06-15 01:28:39 +0200

    Working records

--- tests/src/Main.purs
+++ tests/src/Main.purs
@@ -2,23 +2,23 @@ module Main where
 
 -- import Prelude
 
--- int1 :: Int
--- int1 = 5
+int1 :: Int
+int1 = 5
 
--- int2 :: Int
--- int2 = int1
+int2 :: Int
+int2 = int1
 
--- string1 :: String
--- string1 = "Hello World!"
+string1 :: String
+string1 = "Hello World!"
 
--- char1 :: Char
--- char1 = 'C'
+char1 :: Char
+char1 = 'C'
 
--- array1 :: Array Int
--- array1 = [1, 2, 3, 4]
+array1 :: Array Int
+array1 = [1, 2, 3, 4]
 
--- fun1 :: Int -> Int
--- fun1 arg = arg
+fun1 :: Int -> Int
+fun1 arg = arg
 
 rec1 :: { x :: Int, y :: String }
 rec1 = { x: 1, y: "Hello" }
