85dc5469cc52a629c035dd75275300c173696fa2
Dario Oddenino <branch13@gmail.com>
2020-08-16 18:40:10 +0200

    Top level cases more or less ok

--- src/Language/PureScript/PHP/CodeGen.hs
+++ src/Language/PureScript/PHP/CodeGen.hs
@@ -232,37 +232,41 @@ moduleToPHP (Module _ coms mn _ imps exps foreigns decls) foreign_ =
         | fields /= [] = do
           php <- valueToPHP val []
           withPos ss php
+
       -- top level variables
       -- TODO: Writing this to handle stuff like $this->foo = $this->bar
       -- Probably his approach will break as soon as more complex tests are added.
       -- I think we will need to store the type of PVar somehow.
       -- And then this shouldn't be needed anymore...
-      -- nonRecToPHP (ss, _, _, _) ident (Var _ qi) = do
-      --   let (PVar' _ v) = varToPHP' qi
-      --   -- TODO: identToPHP is ok here?
-      --       acc = (accessorString $ fromString $ T.unpack $ identToPHP ident) (PVar Nothing "this")
-      --       val = (accessorString $ fromString $ T.unpack v) (PVar Nothing "this")
-      --   withPos ss $ PAssignment Nothing acc val
+      nonRecToPHP (ss, _, _, _) ident (Var _ qi) = do
+        let (PVar' _ v) = varToPHP' qi
+        -- TODO: identToPHP is ok here?
+            acc = (accessorString $ fromString $ T.unpack $ identToPHP ident) (PVar Nothing "this")
+            val = (accessorString $ fromString $ T.unpack v) (PVar Nothing "this")
+        withPos ss $ PAssignment Nothing acc val
 
       -- nonRecToPHP (ss, _, _, _) ident val@(Abs (_, _, _, Just IsTypeClassConstructor) _ _) = do
       --   php <- valueToPHP val []
       --   withPos ss (PClass Nothing (Just $ runIdent ident) php)
+
       -- Top level cases.
-      -- nonRecToPHP (ss, _, _, _) ident a@(Abs _ arg val@Case{}) = do
-      --   -- TODO is this phpArg part really needed?
-      --   let phpArg = case arg of
-      --         UnusedIdent -> []
-      --         _           -> [identToPHP arg]
-      --       oscope = updateOScope [] phpArg
-      --   caseBlock <- valueToPHP a oscope
-      --   withPos ss (PMethod Nothing ["public", "static"] (runIdent ident) phpArg caseBlock)
+      nonRecToPHP (ss, _, _, _) ident a@(Abs _ arg val@Case{}) = do
+        -- TODO is this phpArg part really needed?
+        let phpArg = case arg of
+              UnusedIdent -> []
+              _           -> [identToPHP arg]
+            oscope = updateOScope [] phpArg
+        caseBlock <- valueToPHP a oscope
+        -- TODO: this generates bad identation, wait to fix until we check nested cases.
+        withPos ss (PMethod Nothing ["public", "static"] (runIdent ident) phpArg caseBlock)
+
       nonRecToPHP (ss, _, _, _) ident (Abs _ arg val) = do
         let phpArg = case arg of
               UnusedIdent -> []
               _           -> [identToPHP arg]
             oscope = updateOScope [] phpArg
         ret <- valueToPHP val oscope
-        withPos ss (PMethod Nothing ["public", "static"] (runIdent ident) phpArg ret)
+        withPos ss (PMethod Nothing ["public", "static"] (runIdent ident) phpArg (PBlock Nothing True [PReturn Nothing ret]))
 
       nonRecToPHP (ss, _, _, _) ident val = do
         php <- valueToPHP val []
@@ -341,7 +345,6 @@ moduleToPHP (Module _ coms mn _ imps exps foreigns decls) foreign_ =
             oscope' = updateOScope oscope phpArg
         ret <- valueToPHP val oscope'
         return $ PBlock Nothing False [ret]
-        -- return $ PFunction Nothing Nothing phpArg oscope ret -- (PBlock Nothing True [ret])
       valueToPHP' (Abs _ arg val) oscope = do
         let phpArg = case arg of
                         UnusedIdent -> []
--- tests/golden/expected/Cases.php
+++ tests/golden/expected/Cases.php
@@ -5,25 +5,27 @@
 declare(strict_types=1);
 
 class Cases {
-    public static function foo($x) {
-        $__0 = $x;
-        if ($__0 === 1) {
-            return 0;
+    public static function foo($x) 
+        {
+            $__0 = $x;
+            if ($__0 === 1) {
+                return 0;
+            }
+            if ($__0 === 2) {
+                return 1;
+            }
+            return 2;
+            throw new $Error("Failed pattern match at Cases (line 4, column 9 - line 7, column 9): " + [$__0->constructor->name]);
         }
-        if ($__0 === 2) {
-            return 1;
+    public static function bar($x) 
+        {
+            $__1 = $x;
+            if ($__1) {
+                return "True!";
+            }
+            return "False!";
+            throw new $Error("Failed pattern match at Cases (line 10, column 9 - line 10, column 40): " + [$__1->constructor->name]);
         }
-        return 2;
-        throw new $Error("Failed pattern match at Cases (line 4, column 9 - line 7, column 9): " + [$__0->constructor->name]);
-    }
-    public static function bar($x) {
-        $__1 = $x;
-        if ($__1) {
-            return "True!";
-        }
-        return "False!";
-        throw new $Error("Failed pattern match at Cases (line 10, column 9 - line 10, column 40): " + [$__1->constructor->name]);
-    }
 }
 
 ?>
